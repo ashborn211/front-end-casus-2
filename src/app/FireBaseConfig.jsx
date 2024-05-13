c4e1422c603f4c59f0a2b5406a282f66f7f70002
velican <120579492+ashborn211@users.noreply.github.com>
2024-05-13 11:25:47 +0200

    Update FireBaseConfig.jsx

--- src/app/FireBaseConfig.jsx
+++ src/app/FireBaseConfig.jsx
@@ -7,14 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDVxAIl41Ep8QSCCgmL3fgyHh048NkNfkU",
-  authDomain: "front-end-2-7f139.firebaseapp.com",
-  databaseURL: "https://front-end-2-7f139-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "front-end-2-7f139",
-  storageBucket: "front-end-2-7f139.appspot.com",
-  messagingSenderId: "220433614630",
-  appId: "1:220433614630:web:c3c605400f54849adfe3e7",
-  measurementId: "G-PDJT1YB164"
+  apiKey: "AIzaSyBxxp0cLGHEUWRNCy8kwtCvdLPWtPx72qA",
+  authDomain: "front-end-2-11ae4.firebaseapp.com",
+  projectId: "front-end-2-11ae4",
+  storageBucket: "front-end-2-11ae4.appspot.com",
+  messagingSenderId: "997355651902",
+  appId: "1:997355651902:web:0560c3c2d0ad1cca53a26f",
+  measurementId: "G-V4VHVHDMNV"
 };
 
 // Initialize Firebase
