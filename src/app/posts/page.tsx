bd7c76f678e0a1773ed50a80109b2a981aab0f7e
velican <120579492+ashborn211@users.noreply.github.com>
2024-05-29 14:06:03 +0200

    display all posts with the right img

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,12 +1,12 @@
 "use client";
 import '../components/componentStyles.css';
-import UploadImage from "../components/UploadImage";
+import PostsComponent from "../components/PostDisplay";
 
 const upload = () => {
   return (
     <div>
       <h1>Upload Page</h1>
-      <UploadImage />
+      <PostsComponent />
     </div>
   );
 };
