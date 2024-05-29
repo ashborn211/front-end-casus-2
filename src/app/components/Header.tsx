b6320ac56aa65b920fb585340a499c87acc15079
Jakub Flaczynski <jakubflaczynski@gmail.com>
2024-05-29 13:29:46 +0200

    structure

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,11 @@
+import "./componentStyles.css";
+export default function Header() {
+  return (
+    <header>
+      <div className="logo">
+        <h1>MyChan</h1>
+        <p>~ World's best</p>
+      </div>
+    </header>
+  );
+}
--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,15 +1,11 @@
+import Header from "../components/Header";
 
-
-const home = () => {
-
+const Home = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="bg-white p-6 rounded shadow-md w-full max-w-md">
-        <h1 className="text-2xl font-bold mb-4">Login</h1>
-        
-      </div>
-    </div>
+    <main>
+      <Header />
+    </main>
   );
 };
 
-export default home;
+export default Home;
