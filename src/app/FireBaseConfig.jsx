54502c050293e7945bbda8e4b9703f3aaf84bf43
velican <120579492+ashborn211@users.noreply.github.com>
2024-06-10 12:00:16 +0200

    niks

--- src/app/FireBaseConfig.jsx
+++ src/app/FireBaseConfig.jsx
@@ -1,8 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAuth } from 'firebase/auth';
-
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getStorage, ref } from "firebase/storage";
 // Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -22,5 +22,9 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
+const storage = getStorage();
+const storageRef = ref(storage);
+const provider = new GoogleAuthProvider();
 
-export { auth, db };
+
+export { auth, provider, db, storage };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,18 +2,17 @@
 "use client";
 
 import { useState } from "react";
-import { auth } from "./FireBaseConfig";
-import { signInWithEmailAndPassword } from "firebase/auth";
 import { useRouter } from "next/navigation";
+import { auth, provider, db } from "./FireBaseConfig";
+import { signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
+import { doc, setDoc } from "firebase/firestore";
 import Header from "./components/Header";
-import { Main } from "next/document";
-import  "./login.css";
+import "./login.css";
 import Link from "next/link";
 
-
 const LoginPage = () => {
-  const [email, setEmail] = useState<string>('');
-  const [password, setPassword] = useState<string>('');
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
   const router = useRouter();
 
   const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
@@ -22,12 +21,13 @@ const LoginPage = () => {
     try {
       await signInWithEmailAndPassword(auth, email, password);
       alert("Login successful!");
-      router.push("/home");
+      router.push("/user");
     } catch (error: any) {
       console.error(error.message);
-      // Check if the error message indicates that the user doesn't exist
       if (error.code === "auth/user-not-found") {
-        const register = confirm("User does not exist. Do you want to register?");
+        const register = confirm(
+          "User does not exist. Do you want to register?"
+        );
         if (register) {
           router.push("/register");
         }
@@ -35,52 +35,79 @@ const LoginPage = () => {
     }
   };
 
+  const handleGoogleSignIn = async () => {
+    try {
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+
+      // Ensure the user data is stored in Firestore
+      await setDoc(doc(db, "users", user.uid), {
+        userId: user.uid,
+        email: user.email,
+        profilePicture: user.photoURL,
+      });
+
+      alert("Google Sign-In successful!");
+      router.push("/user");
+    } catch (error: any) {
+      console.error("Error signing in with Google:", error.message);
+    }
+  };
+
   return (
     <main>
-    <><Header/>
-    <form onSubmit={handleLogin}>
-    <div className="container">
-        <h1 className="stroke-text">MyChan</h1>
-        <p>Sign into the world's best webpage!</p>
-        <div className="parent-container">
-  <div className="login-box">
-    <h2>Member Login</h2>
-    
-    <label htmlFor="email">
-      E-Mail:
-      <input
-        type="email"
-        className="form-input"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        required
-      />
-    </label>
-    
-    <label htmlFor="password">
-      Password:
-      <input
-        type="password"
-        className="form-input"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        required
-      />
-    </label>
-    
-    <a href="#">Password forgotten?</a>
-    
-    
-      <Link href="/login" className="login-link">LOGIN</Link>
-    
-    
-    <p><a href="#">Or Sign Up Instead </a></p>
-  </div>
-</div>
+      <>
+        <Header />
+        <form onSubmit={handleLogin}>
+          <div className="container">
+            <h1 className="stroke-text">MyChan</h1>
+            <p>Sign into the world's best webpage!</p>
+            <div className="parent-container">
+              <div className="login-box">
+                <h2>Member Login</h2>
 
+                <label htmlFor="email">
+                  E-Mail:
+                  <input
+                    type="email"
+                    className="form-input"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                  />
+                </label>
 
-    </div>
-    </form></>
+                <label htmlFor="password">
+                  Password:
+                  <input
+                    type="password"
+                    className="form-input"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                  />
+                </label>
+
+                <a href="register">Password forgotten?</a>
+
+                <button type="submit" className="login-link">
+                  LOGIN
+                </button>
+
+                <p>
+                  <a href="register">Or Sign Up Instead </a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </form>
+        <button
+          onClick={handleGoogleSignIn}
+          className="w-full bg-red-500 text-white py-2 rounded hover:bg-red-600"
+        >
+          Sign in with Google
+        </button>
+      </>
     </main>
   );
 };
