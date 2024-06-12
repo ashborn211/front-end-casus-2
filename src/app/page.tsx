c0acbdeb3df5d5ed523031168526f22173246318
Homelessman3000 <126881344+Homelessman300@users.noreply.github.com>
2024-06-12 13:16:56 +0200

    Merge branch 'Edwar' into development

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -88,24 +88,21 @@ const LoginPage = () => {
                 </label>
 
                 <a href="register">Password forgotten?</a>
-
-                <button type="submit" className="login-link">
-                  LOGIN
-                </button>
-
+<div className="button-container">
+  <button onClick={handleGoogleSignIn} className="login-link">Google</button>
+  <button type="submit" className="login-link">LOGIN</button>
+</div>
                 <p>
                   <a href="register">Or Sign Up Instead </a>
                 </p>
+                
               </div>
             </div>
+            
           </div>
+          
         </form>
-        <button
-          onClick={handleGoogleSignIn}
-          className="w-full bg-red-500 text-white py-2 rounded hover:bg-red-600"
-        >
-          Sign in with Google
-        </button>
+        
       </>
     </main>
   );
