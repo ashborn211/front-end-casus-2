f30040dce5c5cec9a309abca055977241f83e816
Jakub Flaczynski <jakubflaczynski@gmail.com>
2024-06-17 11:52:07 +0200

    styling change

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,16 +1,20 @@
 "use client";
-import '../components/componentStyles.css';
+import "../components/componentStyles.css";
 import PostsComponent from "../components/PostDisplay";
 import withAuth from "../components/withAuth";
+import Header from "../components/Header";
+import "../home/home.css";
 
 const upload = () => {
   return (
-    <div>
-      <h1>Upload Page</h1>
-      <PostsComponent />
-    </div>
+    <main>
+      <Header></Header>
+
+      <div className="post-container">
+        <PostsComponent />
+      </div>
+    </main>
   );
 };
-
 
 export default withAuth(upload);
