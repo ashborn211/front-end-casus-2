aeb05ca5843aba1f744e4251b7ec329fc72d90f7
velican <120579492+ashborn211@users.noreply.github.com>
2024-06-26 14:34:28 +0200

    Update page.tsx

--- src/app/upload/page.tsx
+++ src/app/upload/page.tsx
@@ -1,14 +1,18 @@
 "use client";
-import '../components/componentStyles.css';
+import "../components/componentStyles.css";
+import Header from "../components/Header";
 import UploadImage from "../components/UploadImage";
 import withAuth from "../components/withAuth";
 
 const upload = () => {
   return (
+    <main>
     <div>
+      <Header />
       <h1>Upload Page</h1>
       <UploadImage />
     </div>
+    </main>
   );
 };
 
