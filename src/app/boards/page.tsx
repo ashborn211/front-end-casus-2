fcad5e963fa34637508307c15ff7f1978b25aeeb
Homelessman3000 <Edikanawati@gmail.com>
2024-06-26 14:34:49 +0200

    board almsot done missing description

--- src/app/boards/page.tsx
+++ src/app/boards/page.tsx
@@ -5,10 +5,10 @@ import { storage } from '../FireBaseConfig';
 import Header from '../components/Header';
 import "./boards.css"
 
-
 interface Image {
   name: string;
-  url: string;
+  image: string;
+  description: string; 
 }
 
 const UserComponent: React.FC = () => {
@@ -20,13 +20,12 @@ const UserComponent: React.FC = () => {
       const storageRef = ref(storage, 'board/'); // Reference to 'boards' folder in Firebase Storage
       try {
         const listResult = await listAll(storageRef);
-        const downloadURLs = await Promise.all(
-          listResult.items.map(async (itemRef) => {
-            const url = await getDownloadURL(itemRef);
-            return { name: itemRef.name, url };
-          })
-        );
-        setImages(downloadURLs);
+        const imagePromises = listResult.items.map(async (itemRef) => {
+          const url = await getDownloadURL(itemRef);
+          return { name: itemRef.name, image: url, description: `Description for ${itemRef.name}` }; // Add appropriate descriptions here
+        });
+        const fetchedImages = await Promise.all(imagePromises);
+        setImages(fetchedImages);
       } catch (error) {
         console.error('Error listing images:', error);
       } finally {
@@ -43,22 +42,19 @@ const UserComponent: React.FC = () => {
 
   return (
     <main>
-<div>
-<Header></Header>
-<div className="container">
-      {images.map((image) => (
-        <div key={image.name} className="item">
-          <img
-          className='item'
-            src={image.url}
-            alt={image.name}
-            
-          />
-        </div>
-      ))}
-    </div>
-    
-    </div>
+      <Header />
+      <div className="container">
+        {images.map((image) => (
+          <div key={image.name} className="item">
+            <img
+              className='item-image'
+              src={image.image}
+              alt={image.name}
+            />
+            <h1>{image.description}</h1>
+          </div>
+        ))}
+      </div>
     </main>
   );
 };
